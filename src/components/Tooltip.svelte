08bf1153a8b78274b3ab0473430199d4dde3c246
jessvoiture <jessicascarr989@gmail.com>
2024-11-13 20:40:45 +0000

    tooltip is better could be better still

--- src/components/Tooltip.svelte
+++ src/components/Tooltip.svelte
@@ -26,6 +26,17 @@
 	} else {
 		adjustedMouseY = $mouseY - tooltipHeight;
 	}
+
+	function formatDaysToYears(days) {
+		const years = Math.round((days / 365.25) * 10) / 10;
+		return years;
+	}
+
+	function formatDate(dateString) {
+		const date = new Date(dateString);
+		const options = { day: '2-digit', month: 'short', year: 'numeric' };
+		return date.toLocaleDateString('en-GB', options).replace(',', '');
+	}
 </script>
 
 <div
@@ -43,31 +54,36 @@
 		</div>
 	{/if}
 
-	<div class="tooltip-content body-text">
+	<div class="tooltip-content">
 		{#if type == 'artist'}
-			<p>{$hoveredData.artist}</p>
+			<div class="header-main-title">{$hoveredData.artist}</div>
 		{/if}
 
 		{#if type == 'album'}
-			<p>
-				{#if $hoveredData.artist}
-					{$hoveredData.artist},
-				{/if}
-				{$hoveredData.album}
-			</p>
-			<p>
-				{$hoveredData.album_release_date}
-			</p>
-			<p>
+			<div class="header">
+				<div class="header-main-title">{$hoveredData.album}</div>
+				<div class="header-artist">by {$hoveredData.artist}</div>
+			</div>
+
+			<div class="tooltip-body">
 				{#if $hoveredData.days_since_first_release == 0}
-					Debut album
+					<div class="tooltip-body-date">
+						Debut album, released {formatDate($hoveredData.album_release_date)}
+					</div>
 				{:else}
-					{Math.round(($hoveredData.days_since_first_release / 365.25) * 10) / 10} years since debut
-					album ({$hoveredData.days_since_first_release} days)
-					{Math.round(($hoveredData.days_since_last_release / 365.25) * 10) / 10} years since previous
-					album
+					<div class="tooltip-body-date">
+						Released {formatDate($hoveredData.album_release_date)}
+					</div>
+					<div class="tooltip-calcs">
+						<div class="tooltip-body-text">
+							{formatDaysToYears($hoveredData.days_since_first_release)} years since debut album
+						</div>
+						<div class="tooltip-body-text">
+							{formatDaysToYears($hoveredData.days_since_last_release)} years since previous album
+						</div>
+					</div>
 				{/if}
-			</p>
+			</div>
 		{/if}
 	</div>
 </div>
@@ -78,24 +94,63 @@
 		background-color: #404040;
 		border: 1px solid #646464;
 		border-radius: 8px;
-		padding: 12px;
+		padding: 12px 16px;
 		display: flex;
 		flex-direction: row;
 		gap: 16px;
-		justify-content: center;
-		align-items: center;
+		justify-content: start;
+		align-items: flex-start;
 		box-sizing: border-box;
 	}
 
 	img {
-		width: 100px;
+		width: 120px;
 		height: auto;
 	}
 
-	.tooltip-content p {
+	.tooltip-content {
+		display: flex;
+		flex-direction: column;
+		gap: 24px;
 		margin: 0;
 		padding: 0;
-		color: #cac7be;
 		width: 200px;
+		font-size: 14px;
+	}
+
+	.header {
+		display: flex;
+		flex-direction: column;
+		gap: 4px;
+	}
+
+	.header-main-title {
+		font-size: 18px;
+		color: #dad3c1;
+		font-weight: bold;
+		text-transform: capitalize;
+	}
+
+	.tooltip-body {
+		display: flex;
+		flex-direction: column;
+		gap: 4px;
+	}
+
+	.tooltip-body-date {
+		color: #dad3c1;
+		font-size: 14px;
+		font-weight: bold;
+	}
+
+	.tooltip-body-text,
+	.header-artist {
+		color: #cac7be;
+	}
+
+	.tooltip-calcs {
+		display: flex;
+		flex-direction: column;
+		gap: 2px;
 	}
 </style>
