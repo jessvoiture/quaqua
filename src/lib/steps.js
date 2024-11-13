a15970945b8aa1bfcdc12d68d507891193f423d1
jessvoiture <jessicascarr989@gmail.com>
2024-11-13 20:55:41 +0000

    small update to the steps

--- src/lib/steps.js
+++ src/lib/steps.js
@@ -1,7 +1,7 @@
 export const steps = [
-	`<p>To begin, let's look at when each artist released their albums. The artists are sorted by the date of their debut album, from Frank Sinatra, with his debut album 'The Voice of Frank Sinatra' released on 4 March, 1946, to Ed Sheeran with his debut album '=' released on 9 September, 2011</p>`,
-	`<p>But rather than plot the albums by the date of release, let's look at time between each album's release date and the artist's debut album release date </p>`,
-	`<p>To get a sense the total time an artist was active, let's add bars</p>`,
+	`<p>Let's plot the albums by release date, with artists sorted by the release of their debut albums, from Frank Sinatra's 1946 debut <span class="album-title-text">The Voice of Frank Sinatra</span> to Ed Sheeran's 2011 debut <span class="album-title-text">=</span></p>`,
+	`<p>But rather than plot the albums by the date of release, let's look at time between each album's release date and the artist's debut album release date.</p>`,
+	`<p>To get a sense the total time an artist was active (ie the time between their first and last release), let's add bars</p>`,
 	'<p>Sorting by the total time active allows us to see</p>',
 	'<p>step 3: bar for years active</p>',
 	'<p>step 4: bar for years active / album count</p>'
