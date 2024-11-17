378e43201b005d3b9b9f0e4da4b247522e4eeef6
jessvoiture <jessicascarr989@gmail.com>
2024-11-17 15:20:42 +0000

    update step copy

--- src/lib/steps.js
+++ src/lib/steps.js
@@ -8,7 +8,7 @@ export const steps = [
 	// 3
 	`<p>Sorting by the years active allows us to easily compare the artists' careers.</p>`,
 	// 4 highlight the beatles only
-	'<p><span class="highlighted-step-text">The Beatles</span> are the only artist whose career spans less than 10 years. With just 7 years and 1 month between their first and last album. Astonishing given their success.</p>',
+	'<p><span class="highlighted-step-text">The Beatles</span> are the only artist whose career spans less than 10 years. With just 7 years and 1 month between their first and last album.</p>',
 	// 5 highlight barbs, rs, and elton
 	'<p>At the other end of the spectrum, are legacy acts like <span class="highlighted-step-text">Barbra Streisand</span>, <span class="highlighted-step-text">The Rolling Stones</span>, and <span class="highlighted-step-text">Elton John</span> who have somewhat steadily released albums over time.</p>',
 	// 6 highlight abba, pf, and eags
