b4ee105974e281947c2377fc6ada1ad3d88a906c
jessvoiture <jessicascarr989@gmail.com>
2024-11-18 21:04:51 +0000

    update steps copy

--- src/lib/steps.js
+++ src/lib/steps.js
@@ -8,11 +8,11 @@ export const steps = [
 	// 3
 	`<p>Sorting by the years active allows us to easily compare the artists' careers.</p>`,
 	// 4 highlight the beatles only
-	'<p><span class="highlighted-step-text">The Beatles</span> are the only artist whose career spans less than 10 years. With just 7 years and 1 month between their first and last album.</p>',
+	'<p><span class="highlighted-step-text">The Beatles</span> are the only artist whose career spans less than 10 years, with just 7 years and 1 month between their first and last albums.</p>',
 	// 5 highlight barbs, rs, and elton
 	'<p>At the other end of the spectrum, are legacy acts like <span class="highlighted-step-text">Barbra Streisand</span>, <span class="highlighted-step-text">The Rolling Stones</span>, and <span class="highlighted-step-text">Elton John</span> who have somewhat steadily released albums over time.</p>',
 	// 6 highlight abba, pf, and eags
 	`<p>Some artists released albums after long hiatuses. <span class="highlighted-step-text">ABBA</span> returned with an album after 40 years, <span class="highlighted-step-text">Pink Floyd</span> after 20 years, and the <span class="highlighted-step-text">Eagles</span> after 28 years.</p>`,
 	// 7 highlight artists with debut > 2000
-	'<p>11 of the 40 best selling artists <span class="highlighted-step-text">debuted after 2000</span> and unsurprisingly their careers have been comparitvely shorter.</p>'
+	'<p>11 of the 40 best selling artists <span class="highlighted-step-text">debuted after 2000</span> and thus their careers have been comparitvely shorter.</p>'
 ];
