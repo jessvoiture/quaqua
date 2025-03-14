1da4b74b8cc5b996d75adb1646fc136efdb7bbcb
jessvoiture <jessicascarr989@gmail.com>
2025-03-14 20:47:27 +0000

    paragraph component

---
+++ src/copy/Paragraph.svelte
@@ -0,0 +1,25 @@
+<div class="paragraph-wrapper">
+	<div class="paragraph">
+		<h4>Years Active</h4>
+		<p>testing testing</p>
+	</div>
+</div>
+
+<style lang="scss">
+	.paragraph-wrapper {
+		display: flex;
+		justify-content: center;
+		width: 100%;
+	}
+
+	.paragraph {
+		display: flex;
+		flex-direction: column;
+		gap: 16px;
+	}
+
+	p {
+		margin: 0;
+		@include body-text;
+	}
+</style>
